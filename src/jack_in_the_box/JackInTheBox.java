8f333859ceecb8103e9e68bf4c465aa7891430a5
League Student <league@iMac-17.attlocal.net>
2019-06-18 19:53:06 -0700

    6/18/19
    :sunglasses:

--- src/jack_in_the_box/JackInTheBox.java
+++ src/jack_in_the_box/JackInTheBox.java
@@ -1,5 +1,87 @@
 package jack_in_the_box;
 
-public class JackInTheBox {
+
+import java.applet.AudioClip;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.URL;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JApplet;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class JackInTheBox implements ActionListener {
+	JButton button = new JButton();
+	int pressed = 0;
+	public void showButton() {
+		
+	
+	JFrame frame = new JFrame();
+frame.setVisible(true);
+
+frame.add(button);
+frame.pack();
+button.addActionListener(this);
+
 
 }
+
+private void showPicture(String fileName) {
+
+
+    try {
+         JLabel imageLabel = createLabelImage(fileName);
+         JFrame frame = new JFrame();
+         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         frame.add(imageLabel);
+         frame.setVisible(true);
+         frame.pack();
+    } catch (Exception e) {
+         e.printStackTrace();
+    }
+}
+
+private JLabel createLabelImage(String fileName) {
+    try {
+         URL imageURL = getClass().getResource(fileName);
+         if (imageURL == null) {
+              System.err.println("Could not find image " + fileName);
+              return new JLabel();
+         } else {
+              Icon icon = new ImageIcon(imageURL);
+              JLabel imageLabel = new JLabel(icon);
+              return imageLabel;
+         }
+    } catch (Exception e) {
+         System.err.println("Could not find image " + fileName);
+         return new JLabel();
+    }
+}
+private void playSound(String soundFile) { 
+    try {
+         AudioClip sound = JApplet.newAudioClip(getClass().getResource(soundFile));
+         sound.play();
+    } catch (Exception e) {
+         e.printStackTrace();
+    }
+}@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+if (e.getSource()==button) {
+		pressed++;}
+if (pressed>5) {
+showPicture("JackInTheBox.png");
+createLabelImage("JackInTheBox");
+playSound("homer-woohoo.wav");
+
+}
+
+	
+}
+
+}
+
+
---
+++ src/jack_in_the_box/JackInTheBoxRunner.java
@@ -0,0 +1,9 @@
+package jack_in_the_box;
+
+import jack_in_the_box.JackInTheBox;
+
+public class JackInTheBoxRunner {
+public static void main(String[] args) {
+	new JackInTheBox().showButton();
+}
+}
